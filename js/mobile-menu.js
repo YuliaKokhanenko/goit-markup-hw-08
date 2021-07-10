625087532c06003249bcb07da8e607f055e0d031
Yulia Kokhanenko <tarutina-z.julia@ukr.net>
2021-07-10 14:26:21 +0300

    Create mobile-menu.js

---
+++ js/mobile-menu.js
@@ -0,0 +1,53 @@
+// (() => {
+//   const mobileMenu = document.querySelector('.js-menu-container');
+//   const openMenuBtn = document.querySelector('.js-open-menu');
+//   const closeMenuBtn = document.querySelector('.js-close-menu');
+//   const toggleMenu = () => {
+//   const isMenuOpen =
+//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     mobileMenu.classList.toggle('is-open');
+
+//     const scrollLockMethod = !isMenuOpen
+//       ? 'disableBodyScroll'
+//       : 'enableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
+//   };
+  
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+
+//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//   });
+// })();
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
+
+  menuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
+
+
+// (() => {
+//   document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+//       e.preventDefault();
+//       new FormData(e.currentTarget).forEach((value, name) =>
+//           console.log(`${name}:
+// ${value}`),
+//       );
+//   });
+// })();
